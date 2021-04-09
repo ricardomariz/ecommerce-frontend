318f585f63e26846d283eb66b5b81c6ee8bd9666
Ricardo Mariz <shaaad@gmail.com>
2021-04-08 23:47:17 -0300

    mockup login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,6 +1,22 @@
+import { Link } from 'react-router-dom'
+import { Content, Container } from "./styles"
+
 function Login() {
   return(
-    <h1>Login</h1>
+    <Container>
+        <Content>
+          <h2>Eshop</h2>
+          <input placeholder="E-mail" />
+          <input type="password" placeholder="Password" />
+          <div>
+            <button type="submit"> Sign in </button>
+            <div>
+              or &nbsp; 
+             <Link to="/register">Sign up</Link>
+             </div>
+          </div>
+        </Content>
+    </Container>
   )
 }
 
