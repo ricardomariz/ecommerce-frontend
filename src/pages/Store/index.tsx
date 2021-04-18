da5da943bf48939d3782b8c3578c063f1cffaa64
Ricardo Mariz <shaaad@gmail.com>
2021-04-17 23:15:30 -0300

    created a button to add fixed products. created button inside product view to delete the product

--- src/pages/Store/index.tsx
+++ src/pages/Store/index.tsx
@@ -1,8 +1,9 @@
 
 import { useEffect, useState } from "react"
 import { Header } from "../../components/Header"
+import { useAuth } from "../../providers/authProvider"
 import { api } from "../../services/api"
-import { Container, Content, Product } from "./styles"
+import { Container, Content, Product, Remove } from "./styles"
 
 interface ProductImage {
   image_name: string;
@@ -19,32 +20,61 @@ interface IProduct {
 
 export function Store() {
   const [products, setProducts] = useState<IProduct[]>([])
+  const { user } = useAuth()
 
-  useEffect(() => {
-    async function getProducts() {
-      const response = await api.get('/products')
-      const products = response.data
-      setProducts(products)
+
+  async function getProducts() {
+    const response = await api.get('/products')
+    const products = response.data
+    setProducts(products)
+  }
+
+  async function addProduct() {
+    const product = {
+      name: "Teste",
+      description: "Oldest mobile phone from Apple",
+      price: 1499.99,
+      user_id: user.user.id
+    }
+    try {
+      await api.post('/products', product)
+      await getProducts()
+    } catch (err) {
+      console.log(err.response.data.message)
+    }
+  }
+
+  async function removeProduct(productId: string) {
+    try {
+      await api.delete('/products/' + productId)
+      await getProducts()
+    } catch (err) {
+      console.log(err.response.data.message)
     }
 
+  }
+  useEffect(() => {
     getProducts();
   }, [])
+
 
 
   return (
     <Container>
       <Header />
+      <button onClick={addProduct}>Add Product</button>
       <Content>
         {
           products.map(product => (
             <Product key={product.id}>
-              <img src={product.images[0].image_name} alt={product.name} />
+              <img src={product.images[0]?.image_name ? product.images[0].image_name : 'http://localhost:3333/tmp/images/products/26acb133a00ad27c934385e82c6d55c0-Passaporte-node-js.png'} alt={product.name} />
               <h4>{product.name}</h4>
               <h5>{new Intl.NumberFormat('pt-br', {
                 style: 'currency',
                 currency: 'BRL',
               }).format(product.price)}</h5>
               <button>Add to cart</button>
+              <Remove onClick={() => removeProduct(product.id)}>Remove Product</Remove>
             </Product>
           ))
         }
--- src/pages/Store/styles.ts
+++ src/pages/Store/styles.ts
@@ -3,6 +3,20 @@ import styled from "styled-components";
 export const Container = styled.div`
   max-width: 1120px;
   margin: 0 auto;
+
+  button {
+    background: transparent;
+    border: 0;
+    color: var(--yellow);
+    cursor: pointer;
+
+    transition: filter 0.2s;
+
+    &:hover{
+      filter: brightness(0.5);
+    }
+  }
+
 `
 
 export const Content = styled.div`
@@ -44,12 +58,28 @@ export const Product = styled.div`
       border: 0;
       color: var(--yellow);
       cursor: pointer;
-      margin-bottom: 1rem;
+      margin-bottom: 0.5rem;
 
       transition: filter 0.2s;
 
       &:hover{
         filter: brightness(0.5);
       }
+
     }
+`
+
+export const Remove = styled.button`
+  margin: 0;
+  background: transparent;
+  border: 0;
+  color: var(--yellow);
+  cursor: pointer;
+  font-size: 9px;
+
+  transition: filter 0.2s;
+
+  &:hover{
+    filter: brightness(0.5);
+  }
 `
